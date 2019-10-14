992d7cbd10a5dc3ac87014675628ecee87ae60c3
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2019-10-14 12:07:33 +0530

    Heroku changes

---
+++ server/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+var fs = require('fs');
+
+var nodeModules = {};
+
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod
+  });
+
+module.exports = {
+  entry: './index.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  externals: nodeModules
+}
